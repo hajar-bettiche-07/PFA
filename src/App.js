8e9bb7a92530593cc8a8777a6bd43a395b258020
unknown <robertkodjoezian@gmail.com>
2025-07-03 17:26:44 +0100

    metamask

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import React from 'react';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Home from './pages/Farmer/Home/home';
 
 function App() {
   return (
     <div className="App">
       <Register/>
       {/* <Login/> */}
+      {/* <Home /> */}
     </div>
   );
 }
